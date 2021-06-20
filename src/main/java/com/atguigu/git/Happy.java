f72d37649ae27c1d1070ed81694718846ef22963
tom_glb <goodMorning_glb@atguigu.com>
2021-06-20 17:54:50 +0800

    fdslafjas

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -3,6 +3,6 @@ package com.atguigu.git;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("Test...");
-		System.out.println("hot_fix2...");
+		System.out.println("hot_fix2...hot_fix2...");
 	}
 }
